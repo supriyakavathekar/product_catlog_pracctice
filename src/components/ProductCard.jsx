07e7c6389caff0d2ea48bfa16a23030f13bfc719
supriyakavathekar <supriyakavathekar20@gmail.com>
2023-05-31 15:01:27 +0200

    feat: add selecct by category

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from './ProductCard.module.css';
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
   return (
-  <>
-  
-  <div>
-    {/* <img src={ product.images[0]} alt= { product.title} /> */}
-  </div>
+    <>
+      {/* What we do here in class */}
+      <div className={styles.cardWrapper}>
+        <div className={styles.imageWrapper}
+         style={{backgroundImage: `url(${product.images[0]})`}} >
+          {/* <img src={product.images[0]} alt={product.title} /> */}
 
-  <div>
-    <h2>{product.title}</h2>
-    <p>{product.description}</p>
-    <p>Category: {product.category}</p>
-    <p>Brand: {product.brand}</p>
-    <p>Price: EUR {product.price}</p>
-  </div>
-  <button>Details</button>
-  
-  </>
-  )
+        </div>
+
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Category: {product.category}</p>
+          <p>Brand: {product.brand}</p>
+          <p>Price: EUR {product.price}</p>
+        </div>
+        <button className={styles.actionBtn}>Details</button>
+      </div>
+    </>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,26 +1,73 @@
-import React, { userEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from './ProductCard';
+import styles from './ProductList.module.css';
+
+
 
 function ProductList() {
 
-const [products, setproducts] = useState([])
+    const [products, setproducts] = useState([])
+    const [selectedCategory, setselectedCategory] = useState([])
 
-function userEffecct() {
-    fetch('https://dummyjson/products')
-    .then((data) => {
-        setproducts(data.products);
-        console.log(data.products);
-    }
-    )
-    .catch((error) =>{
-       console.log('Eroor fetcching product', error);
-    }
 
-    )
-    
-}
+
+
+
+    const categories = [...new Set(products.map((product) => product.category))]
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+        .then((response) => response.json())
+        .then((data) => {
+            setproducts(data.products);
+            console.log(data.products);
+        })
+        .catch((error) =>{
+        console.log('Eroor fetcching product', error);
+        })
+        
+    }, [])
+
+    const handleCategorySelect = (event) => {
+      setselectedCategory(event.target.value)
+    }
 
   return (
-    <div><h1>Product List</h1></div>
+    <div className={styles.listWrapper}>
+    
+    <div className={styles.controlsWrapper}>
+        <label htmlFor='category'>Filter by category:</label>
+        <select
+          id='category'
+          className={styles.inputField}
+          value={selectedCategory}
+          onChange={handleCategorySelect}
+        >
+          <option value=''>All</option>
+          { categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+           ))
+          }
+        </select>
+      </div>
+
+<div className={styles.cardsWrapper}>
+    {products
+    
+    .filter((product) => selectedCategory == '' || product.category === selectedCategory)
+    .map((product) => (
+        <ProductCard key ={product.id} product={product} />
+    )
+
+    )}
+    
+    
+    </div>
+    
+    </div>
+    
   )
 }
 
