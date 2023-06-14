20d3a45ae86fdc2586a7de179461fd6927124b15
supriyakavathekar <supriyakavathekar20@gmail.com>
2023-06-14 14:50:39 +0200

    added buy now and bacck button functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import styles from './ProductCard.module.css';
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ product }) {
+
+  const navigate = useNavigate();
+  function openProductPage() {
+    navigate(`/products/${product.id}` , {state: {product}})
+  }
+
+
+
   return (
     <>
       {/* What we do here in class */}
@@ -19,7 +28,10 @@ function ProductCard({ product }) {
           <p>Brand: {product.brand}</p>
           <p>Price: EUR {product.price}</p>
         </div>
-        <button className={styles.actionBtn}>Details</button>
+        <button className={styles.actionBtn}
+        onClick={openProductPage}
+        
+        >Details</button>
       </div>
     </>
   );
